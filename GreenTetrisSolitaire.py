e4903c1d7b5504e2d6bb6ff8fbcaad77280e9d80
NoahTheCorgi <91349811+NoahTheCorgi@users.noreply.github.com>
2021-09-27 00:30:55 -0400

    updated arcade library license notice

--- GreenTetrisSolitaire.py
+++ GreenTetrisSolitaire.py
@@ -1,4 +1,12 @@
-#DISCLAIMER: UPGRADED AND CUSTOMIZED FROM THE TETRIS TEMPLATE FROM ARCADE ACADEMY
+import arcade
+"""
+The License for the Python Arcade Library used in this project is as follows:
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+#UPGRADED AND CUSTOMIZED FROM THE TETRIS TEMPLATE FROM ARCADE ACADEMY
 """
 What was newly implemented from the default template:
 # color is now green only :D
@@ -26,8 +34,6 @@ What was newly implemented from the default template:
 # (not yet) interesting sound effects, whenever there is a collision or, row complete. or tetris!,,,
 # ... ...
 """
-
-import arcade
 import TetrisLevelAI
 import random
 import PIL #Alternative is Pillow, which forked PIL
